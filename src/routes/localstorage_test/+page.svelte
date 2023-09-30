c3d43d0c0c755bba9e582730b56ec98dc4eb5fc8
Paweł Borecki <p.borecki90@gmail.com>
2023-09-30 17:21:13 +0200

    Added two way binding to localstorage store

--- src/routes/localstorage_test/+page.svelte
+++ src/routes/localstorage_test/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 	import { appstate } from '../../state.ts'
 	import { get } from 'svelte/store'
+
+	const onInput = e => ($appstate.user_id = e.target.value);
+
 	export async function load() {
 	}
 </script>
   
 <main class="relative flex flex-col items-center justify-center min-h-screen py-10">
 	localstorage test {$appstate.user_id}
+	<input bind:value={$appstate.user_id} on:input={onInput} />
 </main>
--- src/state.ts
+++ src/state.ts
@@ -5,11 +5,8 @@ import { persisted } from 'svelte-local-storage-store'
 
 export const appstate = persisted('appstate', {
     user_id: uuidv4(),
+    is_candidate: false,
     is_dark_theme: false,
     is_high_contrast_mode: false,
     font_size:1
 });
-// export const user_id = storable('user_id', uuidv4());
-// export const is_dark_theme = storable('is_dark_theme', false);
-// export const is_high_contrast_mode = storable('is_high_contrast_mode', false);
-// export const font_size = storable('font_size', 1);
