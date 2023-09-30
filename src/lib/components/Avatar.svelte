2cd4c50d1fbeaf9a3de4532c27adb6f7cfc7e39e
Kamil Wojtasiński <k.wojtasinski@wp.pl>
2023-09-30 17:44:26 +0200

    [Hackyeah] Added Avatar

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let imageUrl = 'http://placekitten.com/g/200/300';
+    export let imageClass = ''
+    export let imageName = 'Card Title';
+    export let imageWidth = 110;
+    export let imageHeight = 110;
+</script>
+
+<img src={imageUrl} class="rounded-circle {imageClass}" alt="{imageName}" width="{imageWidth}" height="{imageHeight}" />
