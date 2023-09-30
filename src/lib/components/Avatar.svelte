8260adfb560166aca57c5a58442c0095f115bdc6
Kamil Wojtasiński <k.wojtasinski@wp.pl>
2023-09-30 23:45:55 +0200

    [Hackyeah] Avatar improve

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,9 +1,18 @@
 <script>
     export let imageUrl = 'http://placekitten.com/g/200/300';
     export let imageClass = ''
+    export let imageTitle = ''
     export let imageName = 'Card Title';
-    export let imageWidth = 110;
-    export let imageHeight = 110;
+    export let imageWidth;
+    export let imageHeight;
 </script>
 
-<img src={imageUrl} class="rounded-circle {imageClass}" alt="{imageName}" width="{imageWidth}" height="{imageHeight}" />
+{#if imageWidth && imageHeight}
+  <img src={imageUrl} class="rounded-circle {imageClass}" title="{imageTitle}" alt="{imageName}" width={imageWidth} height={imageHeight} />
+{:else if imageWidth}
+  <img src={imageUrl} class="rounded-circle {imageClass}" title="{imageTitle}" alt="{imageName}" width={imageWidth} />
+{:else if imageHeight}
+  <img src={imageUrl} class="rounded-circle {imageClass}" title="{imageTitle}" alt="{imageName}" height={imageHeight} />
+{:else}
+  <img src={imageUrl} class="rounded-circle {imageClass}" title="{imageTitle}" alt="{imageName}" />
+{/if}
