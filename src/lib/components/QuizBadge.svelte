92df81493dbf7e41b42c714c820749b7b23b36f2
Karol Rydwelski <wh0kxf@europe.phoenixcontact.com>
2023-10-01 00:44:45 +0200

    quiz

---
+++ src/lib/components/QuizBadge.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let answer = '';
+</script>
+
+{#if answer === 'Disagree'}
+	<span class="badge bg-primary">{answer}</span>
+{:else if answer === 'No opionion'}
+	<span class="badge bg-secondary">{answer}</span>
+{:else}
+	<span class="badge bg-success">{answer}</span>
+{/if}
+
+<style>
+</style>
